4122532a3234aaaaabb58af9ae0e5d48ca0e7462
Nireeksha <nireeksharajiv@gmail.com>
2025-12-07 18:54:04 +0530

    Set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  base: '/R-and-D-System/',   // 👈 IMPORTANT: repo name with slashes
 })
